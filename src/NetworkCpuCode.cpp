c63926bdabb8cbcfc1ae45f9f79ead913173fe10
Vincent Zhao <vincentzhaorz@gmail.com>
2016-12-08 12:23:31 +0000

    finished input buffer (simplest)

--- src/NetworkCpuCode.cpp
+++ src/NetworkCpuCode.cpp
@@ -3,18 +3,22 @@
 
 #define H 4
 #define W 4
+#define F 2
+#define C 2
 
-float input[H * W];
-float output[H * W];
+#define INPUT_SIZE (H * W * C)
+#define OUTPUT_SIZE (H * W * F * C)
+float input[INPUT_SIZE];
+float output[OUTPUT_SIZE];
 
 int main(int argc, char *argv[]) {
 
-  for (int i = 0; i < H * W; i ++)
+  for (int i = 0; i < INPUT_SIZE; i ++)
     input[i] = (float) i;
 
   Network(input, output);
   
-  for (int i = 0; i < W; i ++)
+  for (int i = 0; i < OUTPUT_SIZE; i ++)
     printf("Output[%6d] = %lf\n", i, output[i]);
 
   return 0;
