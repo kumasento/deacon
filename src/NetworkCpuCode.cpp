1ccb0fa7aeb61c5e6ed0fb3a1788bfba9af1cf5a
Vincent Zhao <vincentzhaorz@gmail.com>
2016-12-09 14:26:38 +0000

    finished weight reading

--- src/NetworkCpuCode.cpp
+++ src/NetworkCpuCode.cpp
@@ -3,21 +3,27 @@
 
 #define H 4
 #define W 4
-#define F 2
+#define F 16
 #define C 3
+#define K 3
 
 #define INPUT_SIZE (H * W * C)
 #define OUTPUT_SIZE (H * W * F)
+#define WEIGHT_SIZE (F * K * K)
 
 float input[INPUT_SIZE];
 float output[OUTPUT_SIZE];
+float weight[WEIGHT_SIZE];
 
 int main(int argc, char *argv[]) {
 
   for (int i = 0; i < INPUT_SIZE; i ++)
-    input[i] = (float) (i);
+    input[i] = (float) 1;
 
-  Network(input, output);
+  for (int i = 0; i < WEIGHT_SIZE; i ++)
+    weight[i] = (float) 1;
+
+  Network(input, weight, output);
   
   for (int i = 0; i < OUTPUT_SIZE; i ++) {
     if (i % (H * W) == 0)
