248698b652157a8d735663fd4b2185e4fe0750f6
Vincent Zhao <vincentzhaorz@gmail.com>
2016-12-12 21:18:19 +0000

    reformatted the code

--- src/NetworkCpuCode.cpp
+++ src/NetworkCpuCode.cpp
@@ -11,24 +11,25 @@
 
 #define INPUT_SIZE (H * W * C)
 #define OUTPUT_SIZE (H * W * F)
-#define WEIGHT_SIZE (F * K * K)
-#define FC_INP_SIZE (N)
+#define CONV_WGT_SIZE (F * K * K)
 #define FC_OUT_SIZE (M)
 #define FC_WGT_SIZE (M * N)
 
-float input[INPUT_SIZE];
-float output[FC_OUT_SIZE];
-float weight[WEIGHT_SIZE];
-
-float fc_wgt[FC_WGT_SIZE];
+float input [INPUT_SIZE];
+float output [FC_OUT_SIZE];
+float conv_wgt [CONV_WGT_SIZE];
+float fc_wgt [FC_WGT_SIZE];
 
 int main(int argc, char *argv[]) {
 
-  for (int i = 0; i < INPUT_SIZE; i ++) input[i] = (float) 1;
-  for (int i = 0; i < WEIGHT_SIZE; i ++) weight[i] = (float) 1;
-  for (int i = 0; i < FC_WGT_SIZE; i ++) fc_wgt[i] = (float) 1;
+  for (int i = 0; i < INPUT_SIZE; i ++) 
+    input[i] = (float) 1;
+  for (int i = 0; i < CONV_WGT_SIZE; i ++)
+    conv_wgt[i] = (float) 1;
+  for (int i = 0; i < FC_WGT_SIZE; i ++)
+    fc_wgt[i] = (float) 1;
 
-  Network(fc_wgt, input, weight, output);
+  Network(conv_wgt, fc_wgt, input, output);
   
   for (int i = 0; i < FC_OUT_SIZE; i ++) {
     if (i % (H * W) == 0)
