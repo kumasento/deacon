4ec8d080aa11007524609dd01384a9c2f46f8172
Vincent Zhao <vincentzhaorz@gmail.com>
2016-12-12 18:04:11 +0000

    finished FC layer and connect it directly to Conv layer

--- src/NetworkCpuCode.cpp
+++ src/NetworkCpuCode.cpp
@@ -1,31 +1,36 @@
 #include <MaxSLiCInterface.h>
 #include "Maxfiles.h"
 
-#define H 28
-#define W 28
-#define F 20
-#define C 1
-#define K 5
+#define H 4
+#define W 4
+#define F 4
+#define C 2
+#define K 3
+#define M 8
+#define N (H * W * F)
 
 #define INPUT_SIZE (H * W * C)
 #define OUTPUT_SIZE (H * W * F)
 #define WEIGHT_SIZE (F * K * K)
+#define FC_INP_SIZE (N)
+#define FC_OUT_SIZE (M)
+#define FC_WGT_SIZE (M * N)
 
 float input[INPUT_SIZE];
-float output[OUTPUT_SIZE];
+float output[FC_OUT_SIZE];
 float weight[WEIGHT_SIZE];
+
+float fc_wgt[FC_WGT_SIZE];
 
 int main(int argc, char *argv[]) {
 
-  for (int i = 0; i < INPUT_SIZE; i ++)
-    input[i] = (float) 1;
+  for (int i = 0; i < INPUT_SIZE; i ++) input[i] = (float) 1;
+  for (int i = 0; i < WEIGHT_SIZE; i ++) weight[i] = (float) 1;
+  for (int i = 0; i < FC_WGT_SIZE; i ++) fc_wgt[i] = (float) 1;
 
-  for (int i = 0; i < WEIGHT_SIZE; i ++)
-    weight[i] = (float) 1;
-
-  Network(input, weight, output);
+  Network(fc_wgt, input, weight, output);
   
-  for (int i = 0; i < OUTPUT_SIZE; i ++) {
+  for (int i = 0; i < FC_OUT_SIZE; i ++) {
     if (i % (H * W) == 0)
       printf("\n");
     printf("Output[%6d] = %lf\n", i, output[i]);
