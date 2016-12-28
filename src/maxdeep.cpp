18630e27be0bbf5f8adc407063407010b69a889b
Vincent Zhao <vincentzhaorz@gmail.com>
2016-12-28 00:10:16 +0000

    updated the build system and now it supports maxdeep build

--- src/maxdeep.cpp
+++ src/maxdeep.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <MaxSLiCInterface.h>
-#include "Maxfiles.h"
+// #include <MaxSLiCInterface.h>
+// #include "Maxfiles.h"
+
+#include "maxdeep.pb.h"
 
 int main(int argc, char *argv[]) {
   std::cout << "Running Simulation ..." << std::endl;
