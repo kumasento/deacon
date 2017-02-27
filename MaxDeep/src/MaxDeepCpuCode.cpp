aaa24f59f59c552b76a7de17c94a721e489e46b1
Vincent Zhao <vincentzhaorz@gmail.com>
2017-02-27 15:23:59 +0000

    add multipumping and basic DSE script

--- MaxDeep/src/MaxDeepCpuCode.cpp
+++ MaxDeep/src/MaxDeepCpuCode.cpp
@@ -7,8 +7,18 @@
 #include "Maxfiles.h"
 
 int main(int argc, char *argv[]) {
-  const int K = 2;
-  const int N = 2;
+  
+  max_file_t *maxfile = MaxDeep_init();
+  max_engine_t *engine = max_load(maxfile, "local:*");
+
+  const uint64_t K = max_get_constant_uint64t(maxfile, "kernelSize");
+  const uint64_t N = max_get_constant_uint64t(maxfile, "numPipes");
+  const uint64_t F = max_get_constant_uint64t(maxfile, "freq");
+
+  printf("K = %ld\n", K);
+  printf("N = %ld\n", N);
+  printf("F = %ld\n", F);
+
   const int data_size = 384 * K * K * N;
 
   uint8_t *data_inp = (uint8_t *) malloc(sizeof(uint32_t) * data_size);
@@ -26,10 +36,28 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  MaxDeep_dramWrite(data_size * sizeof(uint32_t), 0, data_inp);
-  MaxDeep_dramWrite(data_size * sizeof(uint32_t), data_size * sizeof(uint32_t), data_wgt);
-  MaxDeep(data_size);
-  MaxDeep_dramRead(data_size / (K * K) * sizeof(uint32_t), data_size * 2 * sizeof(uint32_t), data_out);
+  MaxDeep_dramWrite_actions_t inp_write_actions;
+  inp_write_actions.param_size_bytes  = data_size * sizeof(uint32_t);
+  inp_write_actions.param_start_bytes = 0;
+  inp_write_actions.instream_fromcpu  = data_inp;
+
+  MaxDeep_dramWrite_actions_t wgt_write_actions;
+  wgt_write_actions.param_size_bytes  = data_size * sizeof(uint32_t);
+  wgt_write_actions.param_start_bytes = data_size * sizeof(uint32_t);
+  wgt_write_actions.instream_fromcpu  = data_wgt;
+
+  MaxDeep_dramRead_actions_t out_read_actions;
+  out_read_actions.param_size_bytes  = data_size / (K * K) * sizeof(uint32_t);
+  out_read_actions.param_start_bytes = data_size * 2 * sizeof(uint32_t);
+  out_read_actions.outstream_tocpu   = data_out;
+
+  MaxDeep_actions_t actions;
+  actions.param_dataSize = data_size;
+
+  MaxDeep_dramWrite_run(engine, &inp_write_actions);
+  MaxDeep_dramWrite_run(engine, &wgt_write_actions);
+  MaxDeep_run(engine, &actions);
+  MaxDeep_dramRead_run(engine, &out_read_actions);
 
   for (int i = 0; i < 16; i += 4) {
     uint32_t val = 0;
---
+++ scripts/DSE.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import multiprocessing as mp
+from subprocess import call
+from termcolor import colored, cprint 
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_BUILD_DIR = os.path.join(ROOT_DIR, '../MaxDeep/build')
+
+class MaxDeepBuildParam(object):
+    def __init__(self, K, N, F, C):
+        self.num_pipes            = N
+        self.kernel_size          = K
+        self.freq                 = F
+        self.multi_pumping_factor = C
+
+    def getParams(self):
+        return [
+            'NUM_PIPES=%d'            % self.num_pipes,
+            'KERNEL_SIZE=%d'          % self.kernel_size,
+            'FREQ=%d'                 % self.freq,
+            'MULTI_PUMPING_FACTOR=%d' % self.multi_pumping_factor
+        ]
+
+def build(params):
+    print 'Running build ...'
+    print 'ROOT_DIR:  %s' % ROOT_DIR
+    print 'BUILD_DIR: %s' % DEFAULT_BUILD_DIR
+    os.chdir(DEFAULT_BUILD_DIR)
+    print 'CURR_DIR:  %s' % os.path.abspath('./')
+    print 'Run make'
+    call(['make', 'build'] + params.getParams())
+
+if __name__ == '__main__':
+    print '%s for MaxDeep' % colored('Design Space Exploration', 'cyan', attrs=['blink', 'reverse'])
+
+    processes = [
+        mp.Process(target=build, args=(MaxDeepBuildParam(4, 6, 100, 1), )),
+        mp.Process(target=build, args=(MaxDeepBuildParam(4, 6, 100, 2), )),
+        mp.Process(target=build, args=(MaxDeepBuildParam(4, 6, 125, 1), )),
+        mp.Process(target=build, args=(MaxDeepBuildParam(4, 6, 125, 2), )),
+        mp.Process(target=build, args=(MaxDeepBuildParam(4, 6, 150, 1), )),
+        mp.Process(target=build, args=(MaxDeepBuildParam(4, 6, 150, 2), ))
+    ]
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
