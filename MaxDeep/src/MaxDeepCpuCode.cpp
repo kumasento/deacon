1608a9f5d565ae38ba6c25cea83fba1391adc1d1
Vincent Zhao <vincentzhaorz@gmail.com>
2017-03-03 13:53:21 +0000

    make parameters flexible - with EngineParameters and InterfaceParam

--- MaxDeep/src/MaxDeepCpuCode.cpp
+++ MaxDeep/src/MaxDeepCpuCode.cpp
@@ -7,21 +7,44 @@
 #include "Maxfiles.h"
 
 int main(int argc, char *argv[]) {
+  printf("\x1B[32mMaxDeep Command Line Program\x1B[0m\n");
   
+  printf("\x1B[32mLoading\x1B[0m maxfile ...\n");
   max_file_t *maxfile = MaxDeep_init();
   max_engine_t *engine = max_load(maxfile, "local:*");
 
-  const uint64_t K = max_get_constant_uint64t(maxfile, "kernelSize");
-  const uint64_t N = max_get_constant_uint64t(maxfile, "numPipes");
-  const uint64_t F = max_get_constant_uint64t(maxfile, "freq");
+  const uint64_t max_conv_height
+    = max_get_constant_uint64t(maxfile, "maxConvHeight");
+  const uint64_t max_conv_width
+    = max_get_constant_uint64t(maxfile, "maxConvWidth");
+  const uint64_t max_conv_num_channels
+    = max_get_constant_uint64t(maxfile, "maxConvNumChannels");
+  const uint64_t max_conv_num_filters
+    = max_get_constant_uint64t(maxfile, "maxConvNumFilters");
+  const uint64_t max_conv_kernel_size
+    = max_get_constant_uint64t(maxfile, "maxConvKernelSize");
 
-  printf("K = %ld\n", K);
-  printf("N = %ld\n", N);
-  printf("F = %ld\n", F);
+  printf("\x1B[32mConstants:\x1B[0m\n");
+  printf("\t- maxConvHeight:      %ld\n", max_conv_height);
+  printf("\t- maxConvWidth:       %ld\n", max_conv_width);
+  printf("\t- maxConvNumChannels: %ld\n", max_conv_num_channels);
+  printf("\t- maxConvNumFilters:  %ld\n", max_conv_num_filters);
+  printf("\t- maxConvKernelSize:  %ld\n", max_conv_kernel_size);
 
-  const int inp_size = 8 * 8 * 3;
-  const int wgt_size = 4 * 4 * 3 * 96;
-  const int out_size = 5 * 5 * 96;
+  const unsigned int conv_height       = 8;
+  const unsigned int conv_width        = 8;
+  const unsigned int conv_num_channels = 3;
+  const unsigned int conv_num_filters  = 96;
+  const unsigned int conv_kernel_size  = 4;
+  const unsigned int inp_size
+    = conv_height * conv_width * conv_num_channels;
+  const unsigned int wgt_size
+    = conv_kernel_size * conv_kernel_size * conv_num_channels * conv_num_filters;
+  const unsigned int out_size
+    = ((conv_height - conv_kernel_size + 1) *
+       (conv_width - conv_kernel_size + 1) *
+       conv_num_channels *
+       conv_num_filters);
 
   uint8_t *data_inp = (uint8_t *) malloc(sizeof(uint32_t) * inp_size);
   uint8_t *data_wgt = (uint8_t *) malloc(sizeof(uint32_t) * wgt_size);
@@ -30,14 +53,14 @@ int main(int argc, char *argv[]) {
   // TODO: write stream encoder and decoder methods to transform the
   // data type
   // (2017-02-24) Ruizhe Zhao
-  for (int i = 0; i < inp_size * 4; i += 4) {
+  for (int i = 0; i < (int) inp_size * 4; i += 4) {
     uint32_t val = i / 4 + 1;
     for (int j = 0; j < 4; j ++) {
       data_inp[i + j] = (val >> (j * 8));
     }
   }
 
-  for (int i = 0; i < wgt_size * 4; i += 4) {
+  for (int i = 0; i < (int) wgt_size * 4; i += 4) {
     uint32_t val = i / 4 + 1;
     for (int j = 0; j < 4; j ++) {
       data_wgt[i + j] = (val >> (j * 8));
@@ -60,14 +83,18 @@ int main(int argc, char *argv[]) {
   out_read_actions.outstream_tocpu   = data_out;
 
   MaxDeep_actions_t actions;
-  // actions.param_dataSize = data_size;
+  actions.param_conv_height       = conv_height;
+  actions.param_conv_width        = conv_width;
+  actions.param_conv_num_channels = conv_num_channels;
+  actions.param_conv_num_filters  = conv_num_filters;
+  actions.param_conv_kernel_size  = conv_kernel_size;
 
   MaxDeep_dramWrite_run(engine, &inp_write_actions);
   MaxDeep_dramWrite_run(engine, &wgt_write_actions);
   MaxDeep_run(engine, &actions);
   MaxDeep_dramRead_run(engine, &out_read_actions);
 
-  for (int i = 0; i < 16; i += 4) {
+  for (int i = (int) (out_size - 10) * 4; i < (int)out_size * 4; i += 4) {
     uint32_t val = 0;
     for (int j = 0; j < 4; j ++)
       val += (data_out[i + j] << (j * 8));
