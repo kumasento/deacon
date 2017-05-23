fa7c3a614ba16437d19b19c06d70e5b8e56b3e40
Vincent Zhao <vincentzhaorz@gmail.com>
2017-05-23 22:09:54 +0100

    created a script to clean up all my space-wasting scratch directories

---
+++ scripts/cleaning_up_scratch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Removing useless scratch files in the build directories
+
+BUILDS_ROOT=/mnt/data/scratch/rz3515/builds
+
+for dir in ${BUILDS_ROOT}/*; do
+  if [ -d ${dir} ]; then
+    if [ -d ${dir}/scratch ]; then
+      echo ${dir}/scratch
+      rm -rf ${dir}/scratch
+    fi
+  fi
+done
