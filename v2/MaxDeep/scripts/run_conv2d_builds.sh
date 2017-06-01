3847b58575022fda8572e1eeed867b86b0a878d8
Vincent Zhao <vincentzhaorz@gmail.com>
2017-06-01 12:38:20 +0100

    finalised version for ASAP

--- v2/MaxDeep/scripts/run_conv2d_builds.sh
+++ v2/MaxDeep/scripts/run_conv2d_builds.sh
@@ -5,8 +5,9 @@
 run_conv2d_build() {
   num_chnl_pipes=$1
   num_fltr_pipes=$2
-  freq=$3
-  num_of_pumps=$4
+  num_read_pipes=$3
+  freq=$4
+  num_of_pumps=$5
 
   printf 'building CONV2D CHNL_PIPES=%3d FLTR_PIPES=%3d NUM_PUMPS=%d FREQ=%3d ...\n' \
     $num_chnl_pipes $num_fltr_pipes $num_of_pumps $freq
@@ -17,6 +18,7 @@ run_conv2d_build() {
     FREQ=$freq \
     NUM_CONV_CHNL_PIPES=$num_chnl_pipes \
     NUM_CONV_FLTR_PIPES=$num_fltr_pipes \
+    NUM_PIPES=$num_read_pipes \
     MULTI_PUMPING_FACTOR=$num_of_pumps \
     KERNEL_SIZE=3 \
     MAX_CONV_HEIGHT=32 \
@@ -49,44 +51,43 @@ run_conv2d_parallel_pipes_builds() {
 }
 
 run_conv2d_multi_pumped_builds() {
-  run_conv2d_build 1 2  100 2 &
-  run_conv2d_build 1 4  100 2 
-  run_conv2d_build 1 2  150 2 &
-  run_conv2d_build 1 4  150 2 
-  run_conv2d_build 1 2  200 2 &
-  run_conv2d_build 1 4  200 2 
-  run_conv2d_build 1 8  100 2 &
-  run_conv2d_build 1 16 100 2 
-  run_conv2d_build 1 32 100 2 &
-  run_conv2d_build 1 64 100 2 
+  # run_conv2d_build 1 2  100 2 &
+  # run_conv2d_build 1 4  100 2 
+  # run_conv2d_build 1 2  150 2 &
+  # run_conv2d_build 1 4  150 2 
+  # run_conv2d_build 1 2  200 2 &
+  # run_conv2d_build 1 4  200 2 
+  # run_conv2d_build 1 8  100 2 &
+  # run_conv2d_build 1 16 100 2 
+  # run_conv2d_build 1 32 100 2 &
+  # run_conv2d_build 1 64 100 2 
+  run_conv2d_build 2  128 2 100 2
+  run_conv2d_build 3  128 4 100 2
 }
 
 run_conv2d_large_builds() {
-  run_conv2d_build 1  128 100 1 &
-  run_conv2d_build 2  128 100 1 &
-  run_conv2d_build 3  128 100 1 &
-  run_conv2d_build 5  128 100 1 
-  run_conv2d_build 10 128 100 1 &
-  run_conv2d_build 15 128 100 1 &
-  run_conv2d_build 30 128 100 1 
+  run_conv2d_build 1  128 1 100 1 &
+  run_conv2d_build 2  128 2 100 1 
+  run_conv2d_build 1  128 1 150 1 
+  run_conv2d_build 2  128 2 150 1 
 }
 
 run_conv2d_very_large_builds() {
-  run_conv2d_build 1 256 100 1
-  run_conv2d_build 1 256 100 2
-  run_conv2d_build 1 256 150 1
-  run_conv2d_build 1 256 150 2
-  run_conv2d_build 1 256 200 1
-  run_conv2d_build 1 256 200 2
+  run_conv2d_build 1 256 1 100 1
+  run_conv2d_build 1 256 1 100 2
+  run_conv2d_build 1 256 1 150 1
+  run_conv2d_build 1 256 1 150 2
+  run_conv2d_build 1 256 1 200 1
+  run_conv2d_build 1 256 1 200 2
 }
 
 run_conv2d_chnl_builds() {
-  run_conv2d_build 1 1 100 1 &
-  run_conv2d_build 2 1 100 1 &
-  run_conv2d_build 1 2 100 1 &
-  run_conv2d_build 2 2 100 1 
-  run_conv2d_build 1 2 100 2 &
-  run_conv2d_build 2 2 100 2 
+  run_conv2d_build 1 1 1 100 1 &
+  run_conv2d_build 2 1 2 100 1 &
+  run_conv2d_build 1 2 1 100 1 &
+  run_conv2d_build 2 2 2 100 1
+  # run_conv2d_build 1 2 1 100 2
+  # run_conv2d_build 2 2 1 100 2 
 }
 export -f run_conv2d_build
 
--- v2/MaxDeep/scripts/run_conv2d_sim.sh
+++ v2/MaxDeep/scripts/run_conv2d_sim.sh
@@ -3,20 +3,22 @@
 run_conv2d_sim() {
   num_chnl_pipes=$1
   num_fltr_pipes=$2
-  multi_pumping_factor=$3
-  bitwidth=$4
+  num_read_pipes=$3
+  multi_pumping_factor=$4
+  bitwidth=$5
   DEBUG=${DEBUG:-false}
 
-  printf "SIM %d %d %d %d\n\n" $num_chnl_pipes $num_fltr_pipes $multi_pumping_factor $bitwidth
+  printf "SIM %d %d %d %d %d\n\n" $num_chnl_pipes $num_fltr_pipes $num_read_pipes $multi_pumping_factor $bitwidth
 
   make -C ../build runsim \
     DESIGN_NAME=CONV2D \
     KERNEL_SIZE=3 \
     NUM_CONV_CHNL_PIPES=$num_chnl_pipes \
     NUM_CONV_FLTR_PIPES=$num_fltr_pipes \
+    NUM_PIPES=$num_read_pipes \
     MULTI_PUMPING_FACTOR=$multi_pumping_factor \
-    MAX_CONV_NUM_CHANNELS=32 \
-    MAX_CONV_NUM_FILTERS=32 \
+    MAX_CONV_NUM_CHANNELS=4 \
+    MAX_CONV_NUM_FILTERS=24 \
     NUM_ITERS=1 \
     DEBUG=$DEBUG
     BITWIDTH=$bitwidth
