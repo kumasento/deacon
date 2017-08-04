49a58a3168cbe82034004dba8c82d4625d9c5588
Vincent Zhao <vincentzhaorz@gmail.com>
2017-08-04 16:49:39 +0100

    [conv] updated conv2d data and maxlabor

--- evaluation/maxlabor/maxlabor.py
+++ evaluation/maxlabor/maxlabor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+import csv
 import os
 import json
 import itertools
 from argparse import ArgumentParser
+from collections import OrderedDict
 
 
 def parse_args():
@@ -18,7 +20,7 @@ def parse_args():
 
 def load_config(config_file):
     with open(config_file, 'r') as f:
-        return json.loads(f.read())
+        return json.loads(f.read(), object_pairs_hook=OrderedDict)
 
 
 def build(name, config):
@@ -31,11 +33,48 @@ def build(name, config):
         args = ['{0}={1}'.format(x, y) for x, y in zip(param_names, param_value)]
         args_list.append(' '.join(args))
 
+    for idx, args in enumerate(args_list):
+        build_args = ['{0}={1}'.format(x, y) for x, y in design['buildParams'].items()]
+        args += ' ' + ' '.join(build_args)
+        args_list[idx] = args
+
     os.chdir('../build/{0}'.format(name))
     for args in args_list:
         cmd = 'make build {0}'.format(args)
         print('>> ' + cmd)
         os.system(cmd)
+
+
+def analyse(name, config):
+    """
+    Analyse and collect data from the builds of design with name = "name"
+    :param name:
+    :param config:
+    :return:
+    """
+    design = config[name]
+
+    param_value_list = design['params'].values()
+    param_names = design['params'].keys()
+
+    for file in design['config']['files']:
+        name = file['name']
+
+        with open('../data/{0}.csv'.format(name), 'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(param_names + ['LUT', 'FF', 'BRAM', 'DSP'])
+
+            for param_values in itertools.product(*param_value_list):
+                print('permutation: {0}'.format(param_values))
+
+                path = file['path'].format(*param_values)
+                path = os.path.join(design['buildParams']['MAXCOMPILER_BUILD_DIR'], path)
+                lineNumber = file['lineNumber']
+
+                with open(path, 'r') as f:
+                    usage = f.readlines()[lineNumber - 1].strip().split()[:4]
+                csvwriter.writerow(param_values + tuple(usage))
+
 
 
 def main():
@@ -46,6 +85,8 @@ def main():
 
     if args.task == 'build':
         build(args.name, config)
+    elif args.task == 'analyse':
+        analyse(args.name, config)
     else:
         raise ValueError('{0} is not a valid task'.format(args.task))
 
