cefcbfeec408d456fab9891e0a91e9a9bf04db7b
Vincent Zhao <vincentzhaorz@gmail.com>
2017-08-22 13:16:55 +0100

    [eval] revised build_small.sh

---
+++ evaluation/build/conv/build_small.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PF=(1 2 4 8)
+PC=(1 2 4 8)
+
+for pf in $PF; do
+    for pc in $PC; do
+        make build PF=$pf PC=$pc PK=1 bitWidth=32 MAXCOMPILER_BUILD_DIR=/mnt/data/scratch/rz3515/builds &
+        make build PF=$pf PC=$pc PK=2 bitWidth=32 MAXCOMPILER_BUILD_DIR=/mnt/data/scratch/rz3515/builds
+    done
+done
