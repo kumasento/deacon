b9c04b9f33c9216b738ea8c01f869ca34c8ff523
Vincent Zhao <vincentzhaorz@gmail.com>
2017-08-25 14:37:27 +0100

    [eval] add NUM_ITERS support

--- evaluation/src/conv/ConvLayerCpuCode.cpp
+++ evaluation/src/conv/ConvLayerCpuCode.cpp
@@ -3,10 +3,27 @@
 #include <cstdlib>
 #include <chrono>
 #include <ctime>
+#include <getopt.h>
 
 #include "Maxfiles.h"
 
 int main(int argc, char *argv[]) {
+
+  char c;
+  
+  uint64_t batch_size = 1;
+  uint64_t num_iters = 1;
+  while ((c = getopt(argc, argv, "i:n:")) != -1)
+    switch (c) {
+      case 'i':
+        num_iters = atoi(optarg);
+        break;
+      case 'n':
+        batch_size = atoi(optarg);
+        break;
+      default:
+        exit(1);
+    }
 
   max_file_t *max_file = ConvLayer_init();
   max_engine_t* engine = max_load(max_file, "*");
@@ -17,9 +34,6 @@ int main(int argc, char *argv[]) {
   uint64_t F = max_get_constant_uint64t(max_file, "conv_F");
   uint64_t K = max_get_constant_uint64t(max_file, "conv_K");
   uint64_t USE_DRAM = max_get_constant_uint64t(max_file, "USE_DRAM");
-  
-  uint64_t batch_size = 1;
-  uint64_t num_iters = 1;
 
   uint64_t ifmap_num_elems = H * W * C * batch_size;
   uint64_t coeff_0_num_elems = F * C *  K * K * batch_size;
