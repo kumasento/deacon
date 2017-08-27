fb6c836c43994df6ce8e8463aa315335d7f77edc
Ruizhe <vincentzhaorz@gmail.com>
2017-08-26 22:54:36 -0700

    [conv] fixed cli arg in single layer

--- evaluation/src/conv_single_layer/ConvSingleLayerCpuCode.cpp
+++ evaluation/src/conv_single_layer/ConvSingleLayerCpuCode.cpp
@@ -3,12 +3,27 @@
 #include <cstdlib>
 #include <chrono>
 #include <ctime>
+#include <getopt.h>
 
 #include "Maxfiles.h"
 
 typedef int16_t data_t;
 
 int main(int argc, char *argv[]) {
+
+  uint64_t batch_size = 1;
+  uint64_t num_iters = 1;
+  while ((c = getopt(argc, argv, "i:n:")) != -1)
+    switch (c) {
+      case 'i':
+        num_iters = atoi(optarg);
+        break;
+      case 'n':
+        batch_size = atoi(optarg);
+        break;
+      default:
+        exit(1);
+    }
 
   max_file_t *max_file = ConvSingleLayer_init();
   max_engine_t* engine = max_load(max_file, "*");
@@ -19,9 +34,6 @@ int main(int argc, char *argv[]) {
   uint64_t F = max_get_constant_uint64t(max_file, "conv_F");
   uint64_t K = max_get_constant_uint64t(max_file, "conv_K");
   uint64_t USE_DRAM = max_get_constant_uint64t(max_file, "USE_DRAM");
-  
-  uint64_t batch_size = 1;
-  uint64_t num_iters = 1;
 
   uint64_t ifmap_num_elems = H * W * C * batch_size;
   uint64_t coeff_0_num_elems = F * C *  K * K * batch_size;
@@ -38,11 +50,11 @@ int main(int argc, char *argv[]) {
 
   ConvSingleLayer_actions_t actions;
   actions.param_batch_size = batch_size;
-  if (!USE_DRAM) {
+#ifndef USE_DRAM
     actions.instream_ifmap = (const data_t *) ifmap;
     actions.instream_coeff_0 = (const data_t *) coeff_0;
     actions.outstream_ofmap = ofmap;
-  }
+#endif 
 
   printf("Running ...\n");
   std::chrono::time_point<std::chrono::system_clock> start, end;
