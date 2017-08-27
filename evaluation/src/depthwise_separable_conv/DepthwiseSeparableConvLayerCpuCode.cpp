076c26a2a000fd2fea5d5111e340f37395b11a66
Ruizhe <vincentzhaorz@gmail.com>
2017-08-27 01:15:59 -0700

    [eval] fixed halting bug in depthwise

--- evaluation/src/depthwise_separable_conv/DepthwiseSeparableConvLayerCpuCode.cpp
+++ evaluation/src/depthwise_separable_conv/DepthwiseSeparableConvLayerCpuCode.cpp
@@ -3,10 +3,28 @@
 #include <cstdlib>
 #include <chrono>
 #include <ctime>
+#include <getopt.h>
 
 #include "Maxfiles.h"
 
+typedef int32_t data_t;
+
 int main(int argc, char *argv[]) {
+  char c;
+
+  uint64_t batch_size = 1;
+  uint64_t num_iters = 1;
+  while ((c = getopt(argc, argv, "i:n:")) != -1)
+    switch (c) {
+      case 'i':
+        num_iters = atoi(optarg);
+        break;
+      case 'n':
+        batch_size = atoi(optarg);
+        break;
+      default:
+        exit(1);
+    }
 
   max_file_t *max_file = DepthwiseSeparableConvLayer_init();
   max_engine_t* engine = max_load(max_file, "*");
@@ -16,19 +34,22 @@ int main(int argc, char *argv[]) {
   uint64_t C = max_get_constant_uint64t(max_file, "conv_C");
   uint64_t F = max_get_constant_uint64t(max_file, "conv_F");
   uint64_t K = max_get_constant_uint64t(max_file, "conv_K");
-  
-  uint64_t batch_size = 1;
-  uint64_t num_iters = 1;
+
+  printf("H = %lu\n", H);
+  printf("W = %lu\n", W);
+  printf("C = %lu\n", C);
+  printf("F = %lu\n", F);
+  printf("K = %lu\n", K);
 
   uint64_t ifmap_num_elems = H * W * C * batch_size;
   uint64_t depthwise_coeff_num_elems = C * K * K * batch_size;
   uint64_t pointwise_coeff_num_elems = C * F * batch_size;
   uint64_t ofmap_num_elems = (H - K + 1) * (W - K + 1) * F * batch_size;
 
-  int32_t *ifmap = (int32_t *) malloc(sizeof(int32_t) * ifmap_num_elems);
-  int32_t *depthwise_coeff = (int32_t *) malloc(sizeof(int32_t) * depthwise_coeff_num_elems);
-  int32_t *pointwise_coeff = (int32_t *) malloc(sizeof(int32_t) * pointwise_coeff_num_elems);
-  int32_t *ofmap = (int32_t *) malloc(sizeof(int32_t) * ofmap_num_elems);
+  data_t *ifmap = (data_t *) malloc(sizeof(data_t) * ifmap_num_elems);
+  data_t *depthwise_coeff_0 = (data_t *) malloc(sizeof(data_t) * depthwise_coeff_num_elems);
+  data_t *pointwise_coeff_0 = (data_t *) malloc(sizeof(data_t) * pointwise_coeff_num_elems);
+  data_t *ofmap = (data_t *) malloc(sizeof(data_t) * ofmap_num_elems);
 
   // for (uint64_t i = 0; i < ifmap_num_elems; i ++)
   //   ifmap[i] = (rand() % 10) - 5;
@@ -37,14 +58,17 @@ int main(int argc, char *argv[]) {
 
   DepthwiseSeparableConvLayer_actions_t actions;
   actions.param_batch_size = batch_size;
-  // actions.instream_ifmap = (const int32_t *) ifmap;
-  // actions.instream_coeff_0 = (const int32_t *) coeff_0;
-  // actions.outstream_ofmap = ofmap;
+#ifndef USE_DRAM
+    actions.instream_ifmap = (const data_t *) ifmap;
+    actions.instream_depthwise_coeff_0 = (const data_t *) depthwise_coeff_0;
+    actions.instream_pointwise_coeff_0 = (const data_t *) pointwise_coeff_0;
+    actions.outstream_ofmap = ofmap;
+#endif 
 
   printf("Running ...\n");
   std::chrono::time_point<std::chrono::system_clock> start, end;
   start = std::chrono::system_clock::now();
-  for (int i = 0; i < num_iters; i ++)
+  for (int i = 0; i < (int) num_iters; i ++)
     DepthwiseSeparableConvLayer_run(engine, &actions);
   end = std::chrono::system_clock::now();
   printf("Done\n");
