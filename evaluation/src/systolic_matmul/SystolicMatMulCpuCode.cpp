61f9af0a3c7ef1835b1ee5a54f72ce9f071e6cb3
Ruizhe <vincentzhaorz@gmail.com>
2017-11-28 11:20:41 -0800

    [TOOL] created a template generator

---
+++ evaluation/src/systolic_matmul/SystolicMatMulCpuCode.cpp
@@ -0,0 +1,33 @@
+/*! This is an example CPU code.
+ */
+#include <iostream>
+
+#include "Maxfiles.h"
+
+typedef uint8_t T;
+
+int main(int argc, char *argv[]) {
+  max_file_t *max_file = SystolicMatMul_init();
+  max_engine_t* engine = max_load(max_file, "*");
+
+  const int N = 1024;
+  T *x, *y, *z;
+
+  x = (T *) malloc(sizeof(T) * N);
+  y = (T *) malloc(sizeof(T) * N);
+  z = (T *) malloc(sizeof(T) * N);
+
+  SystolicMatMul_actions_t actions;
+  // should be revised
+  actions.param_N = N;
+  actions.instream_x = (const T *) x;
+  actions.instream_y = (const T *) y;
+  actions.outstream_z = z;
+
+  SystolicMatMul_run(engine, &actions);
+
+  max_unload(engine);
+  max_file_free(max_file);
+
+  return 0;
+}
