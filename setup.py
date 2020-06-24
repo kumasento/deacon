88f64a6d2427633f749244399495510407bcaa4a
Vincent Zhao <vincentzhaorz@gmail.com>
2020-06-24 17:21:35 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='Pyra',
+      version='0.1.0',
+      description='Pyra: A Python EDSL for programming DNN streaming architecture',
+      author='Ruizhe Zhao',
+      author_email='vincentzhaorz@gmail.com',
+      packages=find_packages(),
+)
